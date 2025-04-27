73e8d3fbdde0ffd64cb648a0e7b9b2ff24a12966
dekoreee <9306930160zrd@gmail.com>
2025-04-27 13:12:47 +0300

    добавил файл версии 1.0

---
+++ my_memory_card.py
@@ -0,0 +1,96 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QButtonGroup, QRadioButton, QPushButton, QLabel)
+
+class Question():
+    def __init__(self, question, right_answer, wrong1, wrong2, wrong3):
+        # все строки надо задать при создании объекта, они запоминаются в свойства
+        self.question = question
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+def chek_answer():
+        RadioGroupBox.hide()
+        AnsGroupBox.show()
+        btn_OK.setText('Следуещий вопрос')
+
+def next_question():
+        RadioGroupBox.hide()
+        AnsGroupBox.show()   
+        btn_OK.setText('Ответить')
+def click_OK():
+    if btn_OK.text() == 'Ответить':
+        chek_answer()
+    else:
+        next_question()
+
+
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle('Memory Card')
+text = QLabel('Какой нацианальности не существует?')
+btn_OK = QPushButton('Ответить')
+
+RadioGroupBox = QGroupBox("Варианты ответов") 
+rbtn_1 = QRadioButton('Энцы')
+rbtn_2 = QRadioButton('Смурфы')
+rbtn_3 = QRadioButton('Чулымцы')
+rbtn_4 = QRadioButton('Алуеты')
+
+RadioGroup = QButtonGroup() # это для группировки переключателей, чтобы управлять их поведением
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+layout_ans1 = QHBoxLayout()
+layout_ans2 = QVBoxLayout() 
+layout_ans3 = QVBoxLayout()
+
+layout_ans2.addWidget(rbtn_1)
+layout_ans2.addWidget(rbtn_3)
+layout_ans3.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_4)
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+RadioGroupBox.setLayout(layout_ans1)
+
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?')
+lb_Correct = QLabel('ответ будет тут!')
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+
+
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+layout_line1.addWidget(text, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line2.addWidget(RadioGroupBox)
+layout_line2.addWidget(AnsGroupBox)
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=2) # кнопка должна быть большой
+layout_line3.addStretch(1)
+AnsGroupBox.hide()
+layout_card = QVBoxLayout()
+
+
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5) # пробелы между содержимым
+
+btn_OK.clicked.connect(click_OK)
+main_win.setLayout(layout_card)
+main_win.resize(400, 300)
+
+
+
+main_win.show()
+app.exec_()
